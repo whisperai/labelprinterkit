0990b4e85d46058674671776ef166193ff4d053c
Shlomo Zippel <shlomo.zippel@gmail.com>
2018-06-26 14:23:54 -0700

    Added ability to print QR codes, added multiline text, small fixes

--- labelprinterkit/items.py
+++ labelprinterkit/items.py
@@ -3,7 +3,7 @@ Objects that can be placed in a label template
 """
 
 from PIL import Image, ImageDraw, ImageFont
-
+import segno
 
 class Text:
     """A simple text item"""
@@ -18,4 +18,43 @@ class Text:
         image = Image.new("1", self.font.getsize(text), "white")
         draw = ImageDraw.Draw(image)
         draw.text((0, 0), text, "black", self.font)
+        return image
+
+class MultiLineText:
+    """A simple text item"""
+    def __init__(self, font: ImageFont = None) -> None:
+        if font:
+            self.font = font
+        else:
+            # fallback to default font
+            self.font = ImageFont.load_default()
+
+    def render(self, text):
+        lines = text.split('\n')
+        images = []
+        for l in lines:
+            image = Image.new("1", self.font.getsize(l), "white")
+            draw = ImageDraw.Draw(image)
+            draw.text((0, 0), l, "black", self.font)
+            images.append(image)
+
+        width = max([i.size[0] for i in images])
+        height = max([i.size[1] for i in images])
+
+        img = Image.new("1", (width, height * len(lines)), "white")
+        for n, i in enumerate(images):
+            img.paste(i, box=(0, n * height, i.size[0], n * height + i.size[1]))
+
+        return img
+
+
+class MicroQR:
+    """A simple QR item"""
+    def __init__(self, width=32, height=32):
+        self.size = (width, height)
+
+    def render(self, text):
+        qr = segno.make_micro(text)
+        image = qr.to_pil(background=None, border=0)
+        image = image.resize(self.size)
         return image
--- labelprinterkit/label.py
+++ labelprinterkit/label.py
@@ -12,6 +12,12 @@ def _coord_add(tup1, tup2):
     return (tup1[0] + tup2[0], tup1[1] + tup2[1])
 
 
+def scale_to_height(img, height):
+    xdim, ydim = img.size
+    xdim = round((height / ydim) * xdim)
+    return img.resize((xdim, height))
+
+
 class Label:
     """Base class for all labels
 
@@ -25,7 +31,7 @@ class Label:
     """
     items = []  # type: list
 
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if not self.items:
             raise ValueError(
                 "A Labels 'items' attribute must contain a list of "
@@ -36,9 +42,12 @@ class Label:
             [item.render(next(arg_it)) for item in line]
             for line in self.items]
 
+        self.padding = kwargs.get("padding", 0)
+        print("Padding: {}".format(self.padding))
+
     @property
     def size(self) -> Tuple[int, int]:
-        width = max(sum(i.size[0] for i in line)
+        width = max(sum(i.size[0] + self.padding for i in line)
                     for line in self._rendered_items)
         height = sum(max(i.size[1] for i in line)
                      for line in self._rendered_items)
@@ -52,6 +61,13 @@ class Label:
             width: Width request
             height: Height request
         """
+
+        # normalize each line to maxheight for that line
+        self._rendered_items = [
+            [scale_to_height(img, 32) for img in line]
+            for line in self._rendered_items
+        ]
+
         size = self.size
         img = Image.new("1", size, "white")
 
@@ -61,16 +77,16 @@ class Label:
             for item in line:
                 box = (*pos, *_coord_add(item.size, pos))
                 img.paste(item, box=box)
-                pos[0] += item.size[0]
+                pos[0] += item.size[0] + self.padding
 
             pos[0] = 0
             pos[1] += max(i.size[1] for i in line)
 
         xdim, ydim = img.size
-        print("presize", xdim, ydim, height)
+        #print("presize", xdim, ydim)
         xdim = round((height / ydim) * xdim)
 
-        print("calcsize", xdim, ydim)
+        #print("calcsize", xdim, height)
         img = img.resize((xdim, height))
 
         img.save("output.png")
